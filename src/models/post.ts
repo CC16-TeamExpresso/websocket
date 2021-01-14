00f5fd1fb32a0fff57035c86abd90460afe2d1bd
Soshi <SoshiK@users.noreply.github.com>
2021-01-14 14:01:07 +0900

    added messages field to post model

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,9 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import Message from "./messages";
 
 const PostModel = new mongoose.Schema(
   {
     email: { type: String, required: true }, //identifier
-    uri: { type: String, required: true}
+    uri: { type: String, required: true},
+    messages: [{type: Schema.Types.ObjectId, ref: Message, required: false}]
   },
   { collection: 'posts' }
 )
